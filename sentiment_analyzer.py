5889b88cd65adbd0a775a2f52f796128c27fee72
Sahil181045 <sahilkadam257@gmail.com>
2022-11-29 00:43:55 +0530

    Final Commit

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -1,25 +1,47 @@
 import pickle
-from sklearn.feature_extraction.text import CountVectorizer
-#model filename
-filename = 'reviews_classifier.sav'
-loaded_classifier = pickle.load(open(filename, 'rb'))
-cv = CountVectorizer(max_features = 500)
-#loading the corpus
-with open('corpus.data', 'rb') as filehandle:
-    # read the data as binary data stream
-    corpus = pickle.load(filehandle)
-#fitting the count vectorizer with the corpus
-cv.fit_transform(corpus)
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 
-print("\n---------------------------------------------------")
-user_input=input("Enter the review of the restaurant:  ")
-test = [user_input]
-test_vec = cv.transform(test)
-val=loaded_classifier.predict(test_vec)[0]
-print("---------------------------------------------------")
-if(val==0):
-    print("The review entered was negative.")    
-    print("The user did not like the restaurant.")
-if(val==1):
-    print("The review entered was positive.")
-    print("The user liked the restaurant.")
+# Loading MNB Classifier and CountVectorizer instance
+classifier = pickle.load(open('reviews_classifier.sav', 'rb'))
+cv = pickle.load(open('count_vectorizer.sav', 'rb'))
+
+# Defining stop words
+stop_words = set(stopwords.words('english'))
+negative_words = ['no', 'not', 'none', 'nothing', 'never', 'neither', 'nor']
+stop_words = stop_words - set(negative_words)
+
+
+# Function to predict user sentiments based on input review
+def predict_sentiment(input_review):
+
+    input_review = re.sub(pattern='[^a-zA-Z]', repl=' ', string=input_review)
+    input_review = input_review.lower()
+    input_review_words = input_review.split()
+    input_review_words = [
+        word for word in input_review_words if not word in stop_words]
+    ps = PorterStemmer()
+    final_review = [ps.stem(word) for word in input_review_words]
+    final_review = ' '.join(final_review)
+
+    temp = cv.transform([final_review]).toarray()
+    return classifier.predict(temp)
+
+
+def main():
+    print("\n-------------------------------------------------------------------------------------")
+    user_review = input("Enter the review of the restaurant:  ")
+    predicition = predict_sentiment(user_review)
+    print("-------------------------------------------------------------------------------------")
+
+    if (predicition == 0):
+        print("The review entered was negative.")
+        print("The user did not like the restaurant.")
+    else:
+        print("The review entered was positive.")
+        print("The user liked the restaurant.")
+
+
+if __name__ == "__main__":
+    main()
